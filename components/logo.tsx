7ca8db340f42d5b2d99691ab0a8e94985059d0aa
Kyran Burraston <kyran.burraston@gmail.com>
2019-08-15 10:57:45 +0200

    feat: Initial commit, scaffold site

---
+++ components/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => (
+  <img 
+  	src="/static/xxx.svg" 
+  	alt="Image of Amsterdam's iconic three crosses"
+  	width="30"
+  />
+)
+
+export default Logo
---
+++ pages/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import Link from 'next/link'
+import Head from 'next/head'
+import Nav from '../components/nav'
+
+const Home = () => (
+  <div className="container">
+    <Head>
+      <title>remote.amsterdam</title>
+    </Head>
+
+    <Nav />
+
+    <div className='hero'>
+
+    </div>
+
+    <p>
+      <Link href="https://remote.amsterdam">remote.amsterdam</Link> aims to build a community for remote workers based in Amsterdam.
+    </p>
+
+    <p>
+      <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🌍</h6>
+      Digital Nomad in Amsterdam? Check out <Link href="https://nomadlist.com/amsterdam">NomadList</Link> for coworking spaces, accommodation and other recommendations.
+    </p>
+
+    <p>
+      <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🚧</h6>
+      This project is a work-in-progress, but if you're interested in joining the <Link href="https://remote.amsterdam">remote.amsterdam</Link> Slack group, leave your email address.
+    </p>
+
+    <style jsx>{`
+      .container {
+        max-width: 800px;
+        margin: 20px auto;
+      }
+      .hero {
+        width: 100%;
+        color: #333;
+      }
+      .title {
+        margin: 0;
+        width: 100%;
+        padding-top: 80px;
+        line-height: 1.15;
+        font-size: 48px;
+      }
+      p {
+        color: #333;
+      }
+    `}</style>
+  </div>
+)
+
+export default Home
