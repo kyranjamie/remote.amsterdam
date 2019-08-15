5c558fc675ab4b16970888b49c8e2253ca9b9608
Kyran Burraston <kyran.burraston@gmail.com>
2019-08-15 11:12:44 +0200

    feat: Add margins

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,28 +11,28 @@ const Home = () => (
 
     <Nav />
 
-    <div className='hero'>
+    <main>
+      <p>
+        <Link href="https://remote.amsterdam">remote.amsterdam</Link> aims to build a community for remote workers based in Amsterdam.
+      </p>
 
-    </div>
+      <p>
+        <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🌍</h6>
+        Digital Nomad in Amsterdam? Check out <Link href="https://nomadlist.com/amsterdam">NomadList</Link> for coworking spaces, accommodation and other recommendations.
+      </p>
 
-    <p>
-      <Link href="https://remote.amsterdam">remote.amsterdam</Link> aims to build a community for remote workers based in Amsterdam.
-    </p>
+      <p>
+        <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🚧</h6>
+        This project is a work-in-progress, but if you're interested in joining the <Link href="https://remote.amsterdam">remote.amsterdam</Link> Slack group, leave your email address.
+      </p>
+    </main>
 
-    <p>
-      <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🌍</h6>
-      Digital Nomad in Amsterdam? Check out <Link href="https://nomadlist.com/amsterdam">NomadList</Link> for coworking spaces, accommodation and other recommendations.
-    </p>
-
-    <p>
-      <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🚧</h6>
-      This project is a work-in-progress, but if you're interested in joining the <Link href="https://remote.amsterdam">remote.amsterdam</Link> Slack group, leave your email address.
-    </p>
 
     <style jsx>{`
       .container {
         max-width: 800px;
         margin: 20px auto;
+        padding: 0 20px;
       }
       .hero {
         width: 100%;
