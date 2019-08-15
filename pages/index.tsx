b8b04678b5b9c8dbf09b2c2ead2c7a38c812e60a
Kyran Burraston <kyran.burraston@gmail.com>
2019-08-15 11:49:46 +0200

    feat: Add section on aims

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,12 +17,17 @@ const Home = () => (
       </p>
 
       <p>
-        <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🌍</h6>
-        Digital Nomad in Amsterdam? Check out <Link href="https://nomadlist.com/amsterdam">NomadList</Link> for coworking spaces, accommodation and other recommendations.
+        <h6 style={{ fontSize: '56px', margin: '42px 0px 0' }}>👩🏼‍💻👨🏽‍💻</h6>
+        The lack of colleagues can be one of the less glamourous features of working remotely. By connecting remote engineers, designers and product people, we hope to maintain these recurring relationships.
       </p>
 
       <p>
-        <h6 style={{ fontSize: '56px', margin: '42px 0px 16px' }}>🚧</h6>
+        <h6 style={{ fontSize: '56px', margin: '42px 0px 0' }}>🌍</h6>
+        Digital Nomad in Amsterdam? <Link href="https://nomadlist.com/amsterdam">Check out NomadList</Link> for coworking spaces, accommodation and other recommendations.
+      </p>
+
+      <p>
+        <h6 style={{ fontSize: '56px', margin: '42px 0px 0' }}>🚧</h6>
         This project is a work-in-progress, but if you're interested in joining the <Link href="https://remote.amsterdam">remote.amsterdam</Link> Slack group, leave your email address.
       </p>
     </main>
@@ -33,6 +38,9 @@ const Home = () => (
         max-width: 800px;
         margin: 20px auto;
         padding: 0 20px;
+      }
+      main > p:first-child {
+        margin-top: 32px;
       }
       .hero {
         width: 100%;
@@ -47,6 +55,7 @@ const Home = () => (
       }
       p {
         color: #333;
+        line-height: 1.6;
       }
     `}</style>
   </div>
